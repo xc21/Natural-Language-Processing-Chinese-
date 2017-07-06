3bd5ad420e10bac0b563f52d023961a6374c7400
xc21 <xc21@rice.edu>
2017-07-06 18:50:44 +0800

    Update Average Approach.py

--- Average Approach.py
+++ Average Approach.py
@@ -113,8 +113,9 @@ noStop2 =  re.split(r'[～，;！。?？、...]', noStop)
 #按照短句计算短句的平均值特征向量
 
 #初始化feature vector
+import numpy as np
 featureVec = np.zeros((200,),dtype="float32")
-commentFeatureVecs = np.zeros((len(noStop2),200),dtype="float32")
+commentFeatureVecs = []
 
 
 for i in range(len(noStop2)):
@@ -125,3 +126,92 @@ for i in range(len(noStop2)):
             featureVec = np.add(featureVec,model[word])
             if wordNum != 0:
                 featureVec = np.divide(featureVec,wordNum)
+                commentFeatureVecs.append(featureVec)
+                
+                
+                
+#按照comment Feature Vecs 来聚类
+#K-means聚类
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+
+#按照k-means k的取值进行迭代并画图
+from scipy.spatial.distance import cdist, pdist
+
+K = range(1,50)
+KM = [KMeans(n_clusters=k).fit(commentFeatureVecs) for k in K]
+centroids = [k.cluster_centers_ for k in KM]
+
+D_k = [cdist(word_vectors, cent, 'euclidean') for cent in centroids] #Computes distance between each word and cluster centroid， for different k
+cIdx = [np.argmin(D,axis=1) for D in D_k] #返回最小距离的index, for different k
+dist = [np.min(D,axis=1) for D in D_k] #返回最小距离, for different k
+avgWithinSS = [sum(d)/word_vectors.shape[0] for d in dist] #计算SSE的均值
+
+# Total with-in sum of square
+wcss = [sum(d**2) for d in dist]
+tss = sum(pdist(word_vectors)**2)/word_vectors.shape[0] #compute pairwise distance
+bss = tss-wcss
+
+#
+# elbow curve,根据图像估测曲线在k=20后趋于平稳
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(K, avgWithinSS, 'b*-')
+ax.plot(K[20], avgWithinSS[20], marker='o', markersize=12, 
+markeredgewidth=2, markeredgecolor='r', markerfacecolor='None')
+plt.grid(True)
+plt.xlabel('Number of clusters')
+plt.ylabel('Average within-cluster sum of squares')
+plt.title('Elbow for KMeans clustering')
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(K, bss/tss*100, 'b*-')
+plt.grid(True)
+plt.xlabel('Number of clusters')
+plt.ylabel('Percentage of variance explained')
+plt.title('Elbow for KMeans clustering')
+
+
+
+#选定k值，再一次聚类建模
+#so, we choose k=13
+num_clusters = 20
+# Initalize a k-means object and use it to extract centroids
+kmeans_clustering = KMeans(n_clusters = num_clusters)
+idx = kmeans_clustering.fit_predict(word_vectors)
+words=(model.wv.index2word)
+wordDict = dict(zip(idx,model.wv.index2word))
+
+
+#按照类目数目，打印出每一类中的词组
+#初始化一个list of lists,然后按照cluster number依次传入
+test = [[] for i in range(0,num_clusters-1)]
+for cluster in range(0,num_clusters-1):
+    for i in range(len(idx)):
+        if idx[i]==cluster:
+            #contentTemp.append()
+            test[cluster].append(words[i])
+
+#打印出每个cluster的内容
+for i in range(0,num_clusters-1):
+    print("Cluster" ,i, test[i]) #固定字符和变量的联合打印
+            
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+                    
