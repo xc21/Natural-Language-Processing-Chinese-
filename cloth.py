5214910c47eddc0c785aabe08182d5da0505b07a
xc21 <xc21@rice.edu>
2017-07-18 16:10:10 +0800

    Update cloth.py

--- cloth.py
+++ cloth.py
@@ -22,8 +22,7 @@ with codecs.open('C:\\Users\\caoxun\\Desktop\\淘宝评论project\\cloth.csv', "
     
 #遍历，输入评论中重复的标点 （空格可以之后再.strip）
 def get_solo(text):
-    duels=[x+x for x in list('。，！?')]
-    #如需增加标点符号,比如问号,直接将list('。，!')换成list('。，!？')即可. 程度
+    duels=[x+x for x in list('。，,！?')]
     for d in duels:
         while d in text:
             text=text.replace(d,d[0])
@@ -95,15 +94,30 @@ for line in lines:
 
 
 #逐句遍历，小短句中是否有function tag，若有，则保留这个小短句
-cmtKeep=x = [[] for i in range(10)]       
+cmtKeep = [[] for i in range(10)]       
 for i in range(len(functTags)):
     for j in range(len(functTags[i])):
         for k in range(len(noStop2)): 
            if functTags[i][j] in noStop2[k].split():
                 cmtKeep[i].append(noStop2[k])
                 
-                
-#下一步： 将cmtKeep上每一个
+
+#下一步： 将cmtKeep上每一个功能词中对应的评论提出，分别聚类训练
+cmt_quality =  ','.join(cmtKeep[0])
+f =codecs.open("C://Users//caoxun//Desktop//淘宝评论project//cmt_quality.txt", "w",'utf-8') 
+f.write(cmt_quality)
+f.close()
+cmt_shipping = ''.join(cmtKeep[1])
+cmt_size = ''.join(cmtKeep[2])
+cmt_style =''.join( cmtKeep[3])
+cmt_price = ''.join( cmtKeep[4])
+cmt_service = ''.join(cmtKeep[5])   
+cmt_description = ''.join(cmtKeep[6])
+cmt_cltGeneral = ''.join(cmtKeep[7])
+cmt_buy = ''.join(cmtKeep[8])
+cmt_pos = ''.join(cmtKeep[9])
+   
+             
 
 #使用word2vector
 from gensim.models import word2vec
@@ -111,42 +125,10 @@ import logging
 #from gensim import corpora
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-sentences = word2vec.Text8Corpus(r"C:\Users\caoxun\Desktop\淘宝评论project\noStop6.txt",)#需要读取成utf-8，先在前面文件写入时设置好
+sentences = word2vec.Text8Corpus(r"C:\Users\caoxun\Desktop\淘宝评论project\cmt_quality.txt",)#需要读取成utf-8，先在前面文件写入时设置好
 #sentences = LoadCorpora('r"C:\Users\caoxun\Desktop\淘宝评论project\segResult3")
 # 训练w2v
 model = word2vec.Word2Vec(sentences,min_count=2, size=200) #200维的词向量
-
-
-model.most_similar(u"偏大", topn=5)
-#[('太大', 0.9695711135864258),
-# ('小', 0.969524085521698),
-# ('这么', 0.9694556593894958),
-# ('一样', 0.9694411754608154),
-# ('到', 0.9694200754165649)]
-
-#结论： “偏大一组”的找相似效果比较好，但其他词语效果欠佳
-
-model.most_similar(u"物流", topn=5)
-#[('，', 0.9996504187583923),
-# ('好', 0.9996373653411865),
-# ('质量', 0.9996233582496643),
-# ('了', 0.999622106552124),
-# ('的', 0.9996165633201599)]
-
-model.most_similar(u"快递", topn=5)
-#[('，', 0.9994956254959106),
-# ('很', 0.999434769153595),
-# ('衣服', 0.9994258880615234),
-# ('的', 0.9994170665740967),
-# ('好', 0.9994134902954102)]
-
-model.most_similar(u"质量", topn=5)
-#[('，', 0.9998703598976135),
-# ('的', 0.9998605251312256),
-# ('好', 0.999846339225769),
-# ('衣服', 0.9998286962509155),
-# ('很', 0.9998286962509155)]
-
 
 #将model中的词和对应向量保存
 model.save('mymodel')
@@ -170,7 +152,20 @@ import re
 #shoes_new_str = ''.join(shoes_new)
 #用正则替换清理标点
 #noStop2 = re.sub(r"[～~，;！。。?？?、？！]+\1", " 。",noStop)
-noStop2 =  re.split(r'[。！，]+', noStop)
+
+#处理文字，把多余的空格替换为逗号
+cmt_quality=re.sub(r'\s{3,}', ', ', cmt_quality)
+#去除一个位置上多余的标点
+if __name__=='__main__':
+    text=cmt_quality
+    cmt_quality=get_solo(text)
+   
+cmt_quality=get_solo(cmt_quality)
+print(cmt_quality)
+
+#re.split(r'[～,?… …、]+', cmt_quality)
+cmt_quality2 = cmt_quality.split(',')
+print(cmt_quality2)
 
 
 #按照短句计算短句的平均值特征向量
@@ -182,11 +177,11 @@ commentFeatureVecs = []
 
 
 #遍历noStop2小短句中的每一句
-for i in range(len(noStop2)):
+for i in range(len(cmt_quality2 )):
     #这个小短句中的词组数 = 空格数-1
-    wordNum = (noStop2[i].count(' ')) -1
+    wordNum = (cmt_quality2 [i].count(' ')) -1
     #遍历当前小短句里的每一个词
-    for word in noStop2[i].split():
+    for word in cmt_quality2 [i].split():
         #如果当前的词在w2v里的词典里
         if word in words:
             featureVec = np.add(featureVec,model[word])
@@ -256,12 +251,14 @@ wordDict = dict(zip(idx,commentFeatureVecs))
 
 #按照类目数目，打印出每一类中的词组
 #初始化一个list of lists,然后按照cluster number依次传入
-test = [[] for i in range(0,num_clusters-1)]
-for cluster in range(0,num_clusters-1):
+#将cmt_quality_list转变为list
+cmt_quality_list=cmt_quality.split()
+test = [[] for i in range(0,5)]
+for cluster in range(0,5):
     for i in range(len(idx)):
         if idx[i]==cluster:
             #contentTemp.append()
-            test[cluster].append(noStop2[i])
+            test[cluster].append(cmt_quality2[i])
 
 #打印出每个cluster的内容
 for i in range(0,num_clusters-1):
@@ -281,16 +278,7 @@ for j in range(0,19):
     
     
     
- #   但是爬虫结果有重复， 
-#Cluster 0 [(' 大小 合适 ', 5), (' 衣服 也 不 咋 地 ', 2), (' 就是 买大 了 点 ', 2), ('   挺 好 的 ', 2), ('   质量 不错 ', 2), (' 一切 都 满意 ', 2), (' 看着 也 不错 ', 2), (' 卖家 给我发 的 是 桔 蓝色 的 ', 2), (' 质量 好   衣服 收到 了 ', 2), (' 给 弟弟 买的 ', 2), (' 还 以为 很 薄 ', 2), (' 他 非常 喜欢 ', 2), ('   衣服 挺不错 的 ', 2), (' 质量 蛮 好   挺 好 的 ', 2), (' 特别 暖和 ', 2), (' 以后 买 衣服 找 这家 了 昂 ', 2), (' 质量 跟 六七十 的 一样 ', 2), (' 我弟 穿着 有点 大 ', 2), (' 是 帮 老弟 买的 ', 2), (' 可以 买 ', 2)]
-#Cluster 1 [(' 失望 失望 ', 2), (' 服务态度 也好 ', 2), (' 跟 图片 一样 ', 2), (' 值这个价 钱 ', 2), (' 原本 想放 着 ', 2), (' 谁 知道 下雨 ', 2), (' 正好 用 ', 2), ('   对得起 这个 价钱 ', 2), (' 尺码 准 颜色 正 棒棒哒 ', 2), (' 换货 麻烦 ', 2), (' 有需要 还会 光顾 哦   好好 很 暖和   衣服 还有 看   感觉 还 可以   这 也 太大 啦   还 行 ', 2), (' 但是 感觉 布料 摸 着 怪怪的   不错   衣服 开线 了 ', 2), (' 款式 简单 大气 ', 2), ('   还好 吧       ', 2), (' 不知道 以后 会 不会 起球   这个 不错 ', 2), (' 但是 要个 L 为什么 那么 大 ？   有些 大 了 … … 摸 起来 感觉 怪怪的   还好 ', 2), (' 气味 重了 ', 2), (' 差不多 就 这价 吧   先 收货   漂亮 暖和 ', 2), (' 好评   看起来 不错 五星 好评 ', 2), (' 样子 如图 ', 2)]
-#Cluster 2 [(' 质量 很 好 ', 6), (' 好评 ', 6), (' 质量 也 很 好 ', 4), (' 非常 好 ', 3), (' 质量 好 ', 2), (' 质量 还 可以 ', 2), (' 不错 ', 2), (' 喜欢 ', 2), (' 很 喜欢 ', 2), (' 质量 不错 ', 2), (' 值 了 ', 2), (' 挺厚 的 ', 2), (' 很 合适 ', 2), (' 衣服 也 很 合身 ', 2), (' 非常 非常 的 好 ', 2), (' 好 哦 ', 2), (' 质量 很 不错 ', 2), (' 质量 也 不错 ', 2), (' 物有所值 ', 1)]    
-    
-    
-    
-    
-    
-    
+
     
     
                     
