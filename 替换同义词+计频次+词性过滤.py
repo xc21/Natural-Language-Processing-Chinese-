7a10c9a194f91d1c0037c00cc473b9a4a2b37813
xc21 <xc21@rice.edu>
2017-08-11 16:22:54 +0800

    Add files via upload

---
+++ 替换同义词+计频次+词性过滤.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 26 11:42:44 2017
+
+@author: caoxun
+"""
+#介绍：
+#尝试了在分词、去停后替换同义词，输出记录频次最高的备选词，再按照词性过滤一次，去掉实际意义不大的词性
+
+#但是以词性为导向来挑标签，需要大量积累错误，手动矫正结巴分词中的词性分类
+#并且，保留额词语结构不一定，比如很多的n+adj词组可为标签，但是很多的n却经常为价值较低的”关键词“     
+
+import codecs
+import jieba
+import re  
+
+
+#读取数据   
+shoes = codecs.open('C:\\Users\\caoxun\\Desktop\\淘宝评论project\\鞋子评论3.csv', mode='r', errors='strict', buffering=1)
+
+#初始化    
+shoes_new =""
+
+#合并行
+for line in shoes.readlines():
+    shoes_new += "".join(line.split('\n'))
+    
+#去除标点
+shoes_new = re.sub(',','',shoes_new)
+shoes_new = re.sub('[～，;！。?？、╯﹏╰xgjjjnbgggf!,~&middot&middot]','',shoes_new)
+
+
+#导入人工定义的字典,提高分词精准率
+jieba.load_userdict('C://Users//caoxun//Desktop//淘宝评论project//newDict.txt')
+#分词
+segList = jieba.cut(shoes_new,cut_all=False) #=False > 精准模式
+
+#分词结果储存
+segResult = " ".join(segList)
+
+
+#将停用词txt读取成为string变量
+with open('C://Users//caoxun//Desktop//淘宝评论project//Chinese Stop Words.txt', 'r') as myfile:
+    stopwords=myfile.read().replace('\n', '')
+      
+      
+#删除停用词
+segList = segResult.split()
+stopwordsSet = set(stopwords)
+noStop=[seg for seg in segList if seg not in stopwordsSet]
+noStop= " ".join(noStop)
+
+
+#输出分词、去停结果为txt
+f = open("C://Users//caoxun//Desktop//淘宝评论project//noStop3.txt", "w")
+f.write(noStop)
+f.close()
+
+#同义词替换
+file = codecs.open('C://Users//caoxun//Desktop//淘宝评论project//Synonyms.txt', 'r', encoding="utf8")
+syn = []
+with open('C://Users//caoxun//Desktop//淘宝评论project//Synonyms.txt', 'r', encoding="utf8") as file:
+     lines = [line for line in file.read().split('\n')] #分行处理，一行一位
+for line in lines:
+    syn.append(line.split())
+    
+noStop_list = noStop.split()
+for i in range(len(noStop_list)):
+    for j in range(len(syn)):
+        if noStop_list[i] in syn[j][1:]:
+            noStop_list[i] = syn[j][0]            
+
+#将替换完同义词的noStop转回string,写成txt,测试
+noStop_string = " ".join(noStop_list)
+f = open("C://Users//caoxun//Desktop//淘宝评论project//synSub.txt", "w")
+f.write( noStop_string )
+f.close()
+
+#找到出现频率最高的数个词
+words = re.findall(r'\w+', noStop_string) 
+from collections import Counter 
+freqcount = Counter(words).most_common(20)
+print( freqcount)  
+
+#提高频的词，去掉次数
+freqwords = []
+for j in range(0,19):
+       freqwords.append(freqcount[j][0])
+
+        
+
+#标注词性
+freqwords_string = " ".join(freqwords)
+import jieba.posseg as pseg
+words = pseg.cut(freqwords_string)
+
+tags =[]
+#去除 d(副词？)，动词v
+delParts = ["d","v"]
+for w in words:
+    print("%s %s"%(w.word, w.flag))
+    if (str(w.flag) not in delParts):
+        tags.append(str(w.word)) 
+        "".join(tags)
+            
+#运行结果
+#"".join(tags)
+#Out[560]: '好看舒适 鞋子   物流快 鞋口偏紧 正品     买的  以 穿起来 质量 评论 挺舒服 老公'
+   
+
+
+
+
+
